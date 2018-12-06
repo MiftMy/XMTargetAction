da082cebbd26662463c84a3d63aabd4f41368757
梁小迷 <mifit@liangxiaomidemacbook-pro.local>
2018-12-06 11:17:13 +0800

    Initial Commit

---
+++ XMTargetAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMTargetAction
+//
+//  Created by 梁小迷 on 6/12/18.
+//  Copyright © 2018年 mifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMTargetAction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMTargetAction
+//
+//  Created by 梁小迷 on 6/12/18.
+//  Copyright © 2018年 mifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
